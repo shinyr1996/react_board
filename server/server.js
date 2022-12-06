305f5af691cc77069d778a1f3bcdb35246c6526d
ShinYoungRyul <tlsdudfuf124@naver.com>
2022-12-06 21:05:48 +0900

    DB연결세팅

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express"); 
+const app = express();
+const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql"); // mysql 모듈 사용
+
+var connection = mysql.createConnection({
+    host : "localhost",
+    user : "root", //mysql의 id
+    password : "1234", //mysql의 password
+    database : "react", //사용할 데이터베이스
+});
+
+connection.connect();
